17a2fb11b2f900ba187838f2d11b0910ac2ddbfb
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-01-30 04:55:43 -0700

    added onload state tests for password-generator

--- cypress/e2e/password-generator.spec.js
+++ cypress/e2e/password-generator.spec.js
@@ -15,6 +15,55 @@ describe("Password Generator", () => {
     counter++
 
     it(`Test ${counter}: Elements onload`, () => {
+        const labels = [
+            'Password Length',
+            'Include uppercase letters',
+            'Include lowercase letters',
+            'Include numbers',
+            'Include symbols'
+        ]
+        let i = 0
+
+        // @h2
+        cy.get('.container > h2')
+        .should('contain', 'Password Generator')
+        .as('h2')
+
+        // @result
+        cy.get('.result-container > #result')
+        .as('result')
+
+        // @clipboard
+        cy.get('.result-container > button > i')
+        .as('clipboard')
+
+        cy.get('.settings')
+        .find('.setting')
+        .then( jqdiv => {
+            cy.wrap(jqdiv)
+            .find('label')
+            .contains(labels[i])
+
+            i++
+        })
+
+        cy.get('input[type="number"]#length')
+        .should('have.attr', 'type', 'number')
+        .should('have.attr', 'min', '4')
+        .should('have.attr', 'max', '20')
+        .should('have.attr', 'value', '20')
+
+        cy.get('.settings')
+        .find('input[type="checkbox"]')
+        .then( jqcheckbox => {
+            cy.wrap(jqcheckbox)
+            .should('have.attr','type','checkbox')
+            .should('have.attr','checked')
+        })
+
+        // @generate
+        cy.get('.container > button#generate')
+        .should('contain', 'Generate Password')
     })
     counter++
 
